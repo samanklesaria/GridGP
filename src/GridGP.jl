ec77114b35296c0ddf457ec582cd77f2c9095898
Sam Anklesaria <anklesaria@utexs.edu>
2024-03-27 13:20:01 -0500

    Use GPytorch algorithm

--- src/GridGP.jl
+++ src/GridGP.jl
@@ -1,13 +1,14 @@
 module GridGP
+using AbstractGPs: PosteriorGP
 export GridApprox, CG_GP
 
 using ToeplitzMatrices, AbstractGPs, Statistics, FFTW,
-FillArrays, IterativeSolvers, KernelFunctions,
+FillArrays, IterativeSolvers, KernelFunctions, LinearAlgebra,
 SparseArrays, SparseArrays, Interpolations, ChainRulesCore
 import LinearMaps: LinearMap
 import Distributions
 
-include("slq.jl")
+# Simple Kernels are stationary, and should result in Toeplitz matrices.
 
 KernelFunctions.kernelmatrix(k::KernelFunctions.SimpleKernel, x::AbstractRange) = 
   SymmetricToeplitz(k.(x[1], x))
@@ -23,21 +24,41 @@ function KernelFunctions.kernelmatrix(k::KernelFunctions.SimpleKernel,
   end
 end
 
+"""
+A `GridApprox` overloads `kernelmatrix` to interpolate
+on a grid of inducing points. The type paramters are as follows:
+- `T` is the type of the underlying kernel. It must be a kernel tensor
+product of some kind, with each kernel in the product handling a
+different dimension in the grid of inducing points.
+- `G` is the grid type (a vector of some kind of `AbstractRange`).
+- `L` is the type of the Kronecker structured inducing
+point covariance matrix.
+- `I` is an interoplation object.
+"""
 struct GridApprox{T,R,L,I,D} <: Kernel
   k::T
-  g::Grid{R}
+  g::R
   k_uu::L
   itp::I
-  dims::D
+  dims::Vector{Int}
 end
 
-function GridApprox(k, g)
+"""
+Construct a `GridApprox`. 
+Each kernel in `k` acts on a different dimension of the grid. 
+Grid points in dimension `i are spaced according to the `AbstractRange` `g[i]`.
+"""
+function GridApprox(k::KernelTensorProduct, g::Vector{T}) where {T <: AbstractRange}
   dims = length.(g)
   itp = interpolate(Zeros(dims), BSpline(Cubic()))
-  k_uu = kron(LinearMap.(kernelmatrix.(k.kernels, g))...)
+  k_uu = kronecker(kernelmatrix.(k.kernels, g)...)
   GridApprox(k, g, k_uu, Interpolations.itpinfo(itp), dims)
 end
 
+"""
+Construct a sparse matrix that interpolates among the inducing points in `GridApprox`
+to capture the covariance among `xs`.
+"""
 function weights(k::GridApprox, xs::AbstractVector)
   inits = [r[1] for r in k.g]
   steps = eltype(xs[1]).(step.(k.g))
@@ -63,125 +84,130 @@ function KernelFunctions.kernelmatrix(k::GridApprox, xs::AbstractVector)
   W * k.k_uu * W'
 end
 
+"Turn a WeightedIndices into a size `dim` sparse vector of indices"
 function to_sparse(w, dim)
   ixs = clamp.(w.istart : (w.istart + length(w.weights) - 1), 1, dim)
   sparsevec(ixs, collect(w.weights), dim)
 end
 
-struct Lanczos{F}
-  m::Int
-  f::F
+"""
+Uses the algorithm in (Gardner and Pleiss. GPyTorch: Blackbox Matrix-Matrix Gaussian Process
+Inference with GPU Acceleration. NeurIPS 2021) to find GP likelihood.
+"""
+struct Gardner
+    t::Int # Number of batches
+    p::Int # Steps of batched CG
 end
 
-function AbstractGPs.posterior(fx::FiniteGP{Lanczos}, y::AbstractVector)
-    k = fx.f.f.kernel
-    K = kernelmatrix(k, fx.x) + fx.Σy
+struct LOVE
+    α::Vector{Float64}
+    x::Vector{Float64}
+    δ::Vector{Float64}
+    R::Matrix{Float64}
+    R2::Matrix{Float64}
+end
+
+function AbstractGPs.posterior(strat::Gardner, fx::FiniteGP, y::AbstractVector)
+    kern = fx.f.kernel
+    m = size(kern.k_uu, 1)
+    K = LinearMap(kernelmatrix(kern, fx.x)) + fx.Σy
     δ = y - mean(fx)
-    n = norm(δ)
-    V, T = lanczos(K, δ / n, fx.f.m)
-    e1 = sparsevec(1:1, [1.], size(T, 1))
-    α = n * (V * (T \ e1))
-    W = weights(k, xs)
-    R = k.k_uu * (W * V)
-    R2 = T \  R
-    PosteriorGP(fx.f, (α=α, R=R, R2=R2))
+    W = weights(kern, fx.x)
+    b = (W * kern.k_uu * Ones(m)) / m
+    U, Ts, Qs = mbcg(K, [δ; b], Val(true))
+    R = kern.k_uu * (W * Qs[2])
+    R2 = Ts[2] \ R'
+    AbstractGPs.PosteriorGP(fx.f, LOVE(U[:, 1], fx.x, δ, R, R2))
 end
 
-function Statistics.var(g::PosteriorGP{Lanczos}, xs::AbstractVector)
+const LovePosterior = PosteriorGP{UnionAll, Love}
+
+function Statistics.var(g::LovePosterior, xs::AbstractVector)
   W = weights(g.f.kernel, xs)
-  l = RowVecs(W * R) # a x k
-  r = ColVecs(R2 * W') # k x a
+  l = RowVecs(W * g.data.R) # a x k
+  r = ColVecs(g.data.R2 * W') # k x a
   var(g.prior.f, xs) .- dot.(l, r)
 end
 
-function Random.rand(rng::AbstractRNG, fx::FiniteGP{PosteriorGP{Lanczos}} , N::Int)
-  k = fx.f.prior.f.kernel
-  k_uu = k.k_uu 
-  m = fx.f.prior.f.m
+"Lazy application of `vec`"
+struct Vec{K}
+  k::K
+end
+
+"""
+If ``a_i`` and ``b_i`` are the columns of `A` and `B` respectively, this
+is a lazy representation of ``vec\left(\sum_i a_i b^_i^T\right)``.
+"""
+struct OuterSum
+  A::Matrix{Float64}
+  B::Matrix{Float64}
+end
+
+Base.*(v::Vec, s::OuterSum) = dot(vec(v.k * s.A), vec(s.B))
+
+function approx_lml(strat::Gardner, fx::AbstractGPs.FiniteGP, y::AbstractVector)
+    kern = fx.f.kernel
+    K = kernelmatrix(kern, fx.x) + fx.Σy
+    δ = y - mean(fx)
+    return logp_with(strat, δ, Vec(K))
+end
+
+function logp_with(strat, δ, K)
+    n = length(δ)
+    z = randn(n, strat.t)
+    u, Ts = mbcg(K.k, [δ; z])
+    ld = mean([
+        begin
+            F = eigen(T)
+            sum(F.vectors[1, :].^2 .* log.(F.values))
+        end for T in Ts])
+    -n / 2 * log(2 * pi) - 0.5 * (ld + dot(δ, u[:, 1]))
+end
+
+function ChainRulesCore.frule((_, _, _, ΔK), ::typeof(logp_with), strat::Gardner, δ, K)
+    n = length(δ)
+    z = randn(n, strat.t)
+    u, Ts = mbcg(K.k, [δ; z])
+    kxx_inv_δ = u[:, 1]
+    tangent = OuterSum([kxx_inv_δ; z / strat.t], [kxx_inv_δ; u[:, 2:end]])
+    Δresult = ΔK * tangent 
+    ld = mean([
+        begin
+            F = eigen(T)
+            sum(F.vectors[1, :].^2 .* log.(F.values))
+        end for T in Ts])
+    result = -n / 2 * log(2 * pi) - 0.5 * (ld + dot(δ, u[:, 1]))
+    return (result, Δresult)
+end
+
+function ChainRulesCore.rrule(:typeof(logp_with), strat::Gardner, δ, K)
+    n = length(δ)
+    z = randn(n, strat.t)
+    u, Ts = mbcg(K, [δ; z])
+    kxx_inv_δ = u[:, 1]
+    ld = mean(Float64[
+        begin
+            F = eigen(T)
+            sum(F.vectors[1, :].^2 .* log.(F.values))
+        end for T in Ts])
+    result = -n / 2 * log(2 * pi) - 0.5 * (ld + dot(δ, u[:, 1]))
+    function pullback(ΔL)
+       tangent = OuterSum([kxx_inv_δ; z / strat.t], [kxx_inv_δ; u[:, 2:end]])
+       (NoTangent(), NoTangent(), NoTangent(), tangent)
+    end
+    return result, pullback
+end
+
+function Random.rand(rng::AbstractRNG, fx::FiniteGP{LovePosterior} , N::Int)
+  kern = fx.f.prior.kernel
+  k_uu = kern.k_uu 
   A = k_uu - LinearMap(fx.f.data.R) * LinearMap(fx.f.data.R2)
   e1 = sparsevec(1:1, [1.], size(A, 1))
-  V, T = lanczos(A, e1, m)
+  V, T = lanczos(A, e1)
   S = V * cholesky(T).L
-  W = weights(k, fx.x)
-  mean(fx) + W * (S * randn(rng, m, N))
+  W = weights(kern, fx.x)
+  mean(fx) + W * (S * randn(rng, size(S, 1), N))
 end
 
-
-
-# TODO: how does reverse diff with Toeplitz and Kronecker structure
-# work?
-
-## ---
-
-
-function halfkern(k::GridApprox, xs::AbstractVector)
-  W = weights(k, xs)
-  k.k_uu * W1'
-end
-
-struct CG_GP{F} <: AbstractGPs.AbstractGP
-    f::F
-end
-
-Statistics.mean(g::CG_GP, x::AbstractVector) = mean(g.f, x)
-Statistics.cov(g::CG_GP, x::AbstractVector, y::AbstractVector) = cov(g.f, x, y)
-Statistics.var(g::CG_GP, x::AbstractVector) = var(g.f, x)
-
-function AbstractGPs.posterior(fx::AbstractGPs.FiniteGP{CG_GP}, y::AbstractVector{<:Real})
-    k = fx.f.f.kernel
-    K = kernelmatrix(k, fx.x) + fx.Σy
-    δ = y - mean(fx)
-    α = cg(K, δ)
-    h = halfkern(k, fx.x)
-    AbstractGPs.PosteriorGP(fx.f, (α=α, K=K, δ=δ, h=h))
-end
-
-function Statistics.mean(g::PosteriorGP{CG_GP}, xs::AbstractVector)
-  mean(f.prior, x) + weights(g.prior.f.kernel, xs) * g.data.h * g.data.α
-end
-
-
-# TODO: we also should choose the number of points dynamically.
-# Wait until the expectation stabilizes. 
-
-# When evaluating the mean, we want K_*x (W_xu K_uu W_ux)^{-1} y
-# This much is calculated correctly. 
-
-# TODO: we want to cache
-
-# When doing the variance calculation, though, we need access to k_uu by itself. 
-
-
-function slq_logdet(A)
-  slq(pos_log, A, clamp(trunc(Int, size(A, 1) / 4), 1, 16), 16)
-end
-
-function Distributions.logpdf(fx::AbstractGPs.FiniteGP{<:CG_GP}, y::AbstractVecOrMat{<:Real})
-  post = posterior(fx, y)
-  quadform = post.data.α' * post.data.δ
-  ld = slq_logdet(post.data.C)
-  -0.5 * ld -(length(y)/2) * log(2 * pi) - 0.5 * quadform
-end
-
-function ChainRulesCore.frule((_, ΔA), ::typeof(slq_logdet), A)
-  nv = 16
-  m = clamp(trunc(Int, size(A, 1) / 4), 1, 16)
-  rng = default_rng()
-  n = size(A, 1)
-  u = Array{Float64}(undef, n)
-  Γ = 0.0
-  g = 0.0
-  e1 = zeros(n)
-  e1[1] = 1
-  for i in 1:nv
-      rand!(rng, u, (-1, 1))
-      normalize!(u)
-      Q, T = lanczos(A, u, m)
-      g += dot(Q * (T \ e1), ΔA * z)
-      F = eigen(T)
-      Γ += sum(pos_log.(F.values) .* F.vectors[1, :] .^ 2)
-  end
-  (n/nv) * Γ, g / nv
-end
 
 end # module GridGP
